702dc058e4470dc5db7a6c8c7f2a5e13436ccd35
Michcarrau <2302020@utrivieramaya.edu.mx>
2024-10-23 16:21:53 -0500

    first commit

---
+++ JS/String_conca.js
@@ -0,0 +1,8 @@
+let name1 = 'Miche';
+let lastName = 'Carrau';
+let age = 18;
+let space = ' ';
+
+let fullName = name1 + space + lastName + space + age + space;
+
+console.log(fullName)
---
+++ JS/math_object.js
@@ -0,0 +1,34 @@
+const PI = Math.PI
+console.log(PI)
+console.log(Math.round(9.85)) //Devuelve el valor de un número redondeado al número más cercano.
+console.log(Math.floor(PI)) //Devuelve el mayor entero menor que o igual a un número.
+console.log(Math.ceil(PI)) //Devuelve el entero más pequeño mayor o igual que un número.
+console.log(Math.min(-5, 3, 30, 50, 5, 10, 0)) //Hace una busqueda de los números y devuelve el más pequeño.
+console.log(Math.max(-5, 3, 30, 50, 5, 10, 0)) //Ahora pero devuelve el nás grande.
+
+const radNum = Math.random()
+console.log(radNum) //Devuelve un número pseudo-aleatorio entre 0 y 1
+
+const num = Math.floor(Math.random( ) * 11) //Devuelve un número pseudo-aleatorio entre 0 y 10
+console.log(num)
+
+console.log(Math.abs(-10)) //Devuelve el valor absoluto de un numero
+
+console.log(Math.sqrt(100)) //Devuelve la raiz cuadrada positiva de un numero
+
+console.log(Math.sqrt(2))
+
+console.log(Math.pow(3,2)) //Las devoluciones de base a la potencia de exponente, que es, basepexponent
+
+console.log(Math.E)
+
+//Logarithm
+console.log(Math.log(2)) //Devuelve el logaritmo natural de un número 
+console.log(Math.log(10))
+
+//Trigonometry
+console.log(Math.sin(0))
+console.log(Math.sin(60)) //Devuelven el seno de un número
+console.log(Math.cos(0))
+console.log(Math.cos(60))
+
